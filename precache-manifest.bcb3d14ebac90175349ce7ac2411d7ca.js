e4e6d8c0ff80b7c439a9e1be51ddabd8eb7559b2
Denys <denys.cheporniuk@gmail.com>
2019-12-17 17:16:10 +0200

    Updates

---
+++ precache-manifest.bcb3d14ebac90175349ce7ac2411d7ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61abebe47190b2c16d01f323bd12d2a",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "14cf7e15676e2cf7c5f7",
+    "url": "/react_pagination/static/css/main.997ab834.chunk.css"
+  },
+  {
+    "revision": "8c2e050f921c3609b4e7",
+    "url": "/react_pagination/static/js/2.235a48d4.chunk.js"
+  },
+  {
+    "revision": "14cf7e15676e2cf7c5f7",
+    "url": "/react_pagination/static/js/main.171016e5.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
